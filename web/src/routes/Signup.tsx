66f16d2c9ef74da632a94eaaa34e56f83a725b5c
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2021-06-04 17:39:03 -0400

    set loading, etc to signin

--- web/src/routes/Signup.tsx
+++ web/src/routes/Signup.tsx
@@ -7,7 +7,8 @@ import Label from '../components/common/Label'
 import {publicFetch} from '../util/fetch'
 import FormSuccess from '../components/common/FormSuccess'
 import FormError from '../components/common/FormError'
-import {useState} from 'react'
+import {Fragment, useState} from 'react'
+import {Redirect} from 'react-router'
 
 const SignupSchema = Yup.object().shape({
   firstName: Yup.string().required('First name is required'),
@@ -16,89 +17,100 @@ const SignupSchema = Yup.object().shape({
   password: Yup.string().required('Password is required'),
 })
 
-// interface UserCredential {
-//   fistName: string
-//   lastName: string
-//   password: string
-//   email: string
-// }
-
 function Signup() {
   const [signupSuccess, setSignupSuccess] = useState('')
   const [signupError, setSignupError] = useState('')
+  const [redirectOnLogin, setRedirectOnLogin] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
 
   async function submitCredentials(credentials: any) {
     try {
-      console.log(credentials)
+      setIsLoading(true)
       const {data} = await publicFetch.post(`signup`, credentials)
-      console.log({data})
       setSignupSuccess(data.message)
+      setSignupError('')
+
+      setTimeout(() => {
+        setRedirectOnLogin(true)
+      })
     } catch (err) {
-      console.log({err})
       const {data} = err.response
       setSignupError(data.message)
+      setSignupSuccess('')
+    } finally {
+      setIsLoading(false)
     }
   }
 
   return (
-    <Layout>
-      <Heading sx={{textAlign: 'center'}}>Signup Page</Heading>
-      <Container p={4} bg="muted" mt={4}>
-        <Formik
-          initialValues={{
-            firstName: '',
-            lastName: '',
-            email: '',
-            password: '',
-          }}
-          onSubmit={(values) => submitCredentials(values)}
-          // onSubmit={(values) => {
-          //   console.log(values)
-          // }}
-          validationSchema={SignupSchema}
-        >
-          {() => (
-            <Form>
-              {signupSuccess && (
-                <FormSuccess text={signupSuccess || 'success'} />
-              )}
-              {signupError && <FormError text={signupError || 'error'} />}
-              <Box>
-                <Label text="First Name" htmlFor="firstName" />
-                <FormInput
-                  ariaLabel="First Name"
-                  name="firstName"
-                  type="text"
-                />
-              </Box>
-              <Box>
-                <Label text="Last Name" htmlFor="lastName" />
-                <FormInput ariaLabel="Last Name" name="lastName" type="text" />
-              </Box>
-              <Box>
-                <Label text="Email address" htmlFor="email" />
-                <FormInput
-                  ariaLabel="Email address"
-                  name="email"
-                  type="email"
-                />
-              </Box>
-              <Box>
-                <Label text="password" htmlFor="password" />
-                <FormInput
-                  ariaLabel="Password"
-                  name="password"
-                  type="password"
-                />
-              </Box>
-              <Box mt={2}>
-                <Button type="submit">Register</Button>
-              </Box>
-            </Form>
-          )}
-        </Formik>
-      </Container>
-    </Layout>
+    <Fragment>
+      {redirectOnLogin && <Redirect to="/dashboard" />}
+      <Layout>
+        <Heading sx={{textAlign: 'center'}}>Signup Page</Heading>
+        <Container p={4} bg="muted" mt={4}>
+          <Formik
+            initialValues={{
+              firstName: '',
+              lastName: '',
+              email: '',
+              password: '',
+            }}
+            onSubmit={(values) => submitCredentials(values)}
+            validationSchema={SignupSchema}
+          >
+            {() => (
+              <Form>
+                {signupSuccess && (
+                  <FormSuccess text={signupSuccess || 'success'} />
+                )}
+                {signupError && <FormError text={signupError || 'error'} />}
+                <Box>
+                  <Label text="First Name" htmlFor="firstName" />
+                  <FormInput
+                    ariaLabel="First Name"
+                    name="firstName"
+                    type="text"
+                  />
+                </Box>
+                <Box>
+                  <Label text="Last Name" htmlFor="lastName" />
+                  <FormInput
+                    ariaLabel="Last Name"
+                    name="lastName"
+                    type="text"
+                  />
+                </Box>
+                <Box>
+                  <Label text="Email address" htmlFor="email" />
+                  <FormInput
+                    ariaLabel="Email address"
+                    name="email"
+                    type="email"
+                  />
+                </Box>
+                <Box>
+                  <Label text="password" htmlFor="password" />
+                  <FormInput
+                    ariaLabel="Password"
+                    name="password"
+                    type="password"
+                  />
+                </Box>
+                <Box mt={2}>
+                  <Button
+                    type="submit"
+                    disabled={isLoading}
+                    variant={!isLoading ? 'simple' : 'disabled'}
+                  >
+                    {isLoading ? 'Loading...' : 'Register'}
+                  </Button>
+                </Box>
+              </Form>
+            )}
+          </Formik>
+        </Container>
+      </Layout>
+    </Fragment>
   )
 }
 
