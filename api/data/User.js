51cd05241d9039fda8c417a3094f71df4ff2ab3b
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2021-06-04 17:38:15 -0400

    user model fixed

--- api/data/User.js
+++ api/data/User.js
@@ -7,7 +7,7 @@ const userModel = new Schema({
   email: {type: String, required: true},
   password: {type: String, required: true},
   role: {type: String, required: true, default: 'user'},
-  bio: {type: String, required: true},
+  bio: {type: String, required: false},
 })
 
 module.exports = mongoose.model('user', userModel)
