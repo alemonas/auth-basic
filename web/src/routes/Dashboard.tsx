78b50868b86d5ba31e715d0fbc789a9082513ec0
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2021-06-04 17:38:46 -0400

    add dashboard route

---
+++ web/src/routes/Dashboard.tsx
@@ -0,0 +1,12 @@
+import {Heading} from 'theme-ui'
+import Layout from '../components/Layout'
+
+function Dashboard() {
+  return (
+    <Layout>
+      <Heading>Dashboard</Heading>
+    </Layout>
+  )
+}
+
+export default Dashboard
