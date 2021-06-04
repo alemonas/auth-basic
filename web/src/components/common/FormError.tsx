a19a198513343ddb2b4195b565a6ac69b4de8cfe
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2021-06-04 15:46:57 -0400

    add form error custom component

--- web/src/components/common/FormError.tsx
+++ web/src/components/common/FormError.tsx
@@ -1,11 +1,15 @@
-import {Box} from 'theme-ui'
+import {Box, Alert} from 'theme-ui'
 
 interface FormErrorProps {
   text: string
 }
 
 function FormError({text}: FormErrorProps) {
-  return <Box sx={{p: 2}}>{text}</Box>
+  return (
+    <Box mb={2}>
+      <Alert variant="error">{text}</Alert>
+    </Box>
+  )
 }
 
 export default FormError
