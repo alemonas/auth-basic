1bb6c4f672f6559e7751b34aa4ef49cd12e929ef
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2021-06-08 12:34:23 -0400

    use only link from router lib

--- web/src/components/MainNav.tsx
+++ web/src/components/MainNav.tsx
@@ -1,18 +1,21 @@
 import {Link} from 'react-router-dom'
-import {Flex, NavLink} from 'theme-ui'
+import {Flex, Box} from 'theme-ui'
 
 function MainNav() {
   return (
     <Flex as="nav">
-      <NavLink href="#!" p={2}>
+      <Box p={2} variant="links.nav">
         <Link to="home">Home</Link>
-      </NavLink>
-      <NavLink href="#!" p={2}>
+      </Box>
+      <Box p={2}>
         <Link to="signup">Signup</Link>
-      </NavLink>
-      <NavLink href="#!" p={2}>
+      </Box>
+      <Box p={2}>
+        <Link to="login">Login</Link>
+      </Box>
+      <Box p={2}>
         <Link to="dashboard">Dashboard</Link>
-      </NavLink>
+      </Box>
     </Flex>
   )
 }
