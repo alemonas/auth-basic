f244b002d800a51969071af4f57f8b9896f5448f
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2021-06-14 08:58:22 -0400

    wip: refactor navbar, using an array of items

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,39 +1,75 @@
 /** @jsxImportSource theme-ui */
-import {Link} from 'react-router-dom'
+import {Link, useLocation} from 'react-router-dom'
 import {Flex, Box} from 'theme-ui'
 import {authSelector} from '../redux/authSlice'
 import {useAppSelector} from '../redux/hooks'
 
+interface NavItemTypes {
+  label: string
+  path: string
+  allowedRoles: string[]
+}
+
+const navItems: NavItemTypes[] = [
+  {
+    label: 'Home',
+    path: 'home',
+    allowedRoles: ['user'],
+  },
+  {
+    label: 'Singup',
+    path: 'signup',
+    allowedRoles: ['user'],
+  },
+  {
+    label: 'login',
+    path: 'login',
+    allowedRoles: ['user'],
+  },
+  {
+    label: 'Dashboard',
+    path: 'dashboard',
+    allowedRoles: ['user', 'admin'],
+  },
+]
+
+interface NavItemProps {
+  navItem: NavItemTypes
+}
+
+interface NavItemContainerProps {
+  children: React.ReactNode
+}
+
+const NavItem = ({navItem}: NavItemProps) => {
+  const location = useLocation()
+  console.log({location})
+  const isCurrentRoute = location.pathname === `/${navItem.path}`
+  return (
+    <Link
+      sx={{variant: isCurrentRoute ? 'styles.a.current' : 'styles.a'}}
+      to={navItem.path}
+    >
+      {navItem.label}
+    </Link>
+  )
+}
+
+const NavItemContainer = ({children}: NavItemContainerProps) => (
+  <Box p={2}>{children}</Box>
+)
+
 function Navbar() {
-  const {isAuthenticated} = useAppSelector(authSelector)
+  // const {isAuthenticated} = useAppSelector(authSelector)
   return (
     <Flex as="nav" sx={{justifyContent: 'center'}}>
-      <Flex p={2} variant="links.nav">
-        <Link sx={{variant: 'styles.a'}} to="home">
-          Home
-        </Link>
-      </Flex>
-      {!isAuthenticated && (
-        <Box p={2}>
-          <Link sx={{variant: 'styles.a'}} to="signup">
-            Signup
-          </Link>
-        </Box>
-      )}
-      {!isAuthenticated && (
-        <Box p={2}>
-          <Link sx={{variant: 'styles.a'}} to="login">
-            Login
-          </Link>
-        </Box>
-      )}
-      {isAuthenticated && (
-        <Box p={2}>
-          <Link sx={{variant: 'styles.a'}} to="dashboard">
-            Dashboard
-          </Link>
-        </Box>
-      )}
+      {navItems.map((navItem) => {
+        return (
+          <NavItemContainer key={navItem.path}>
+            <NavItem navItem={navItem} />
+          </NavItemContainer>
+        )
+      })}
     </Flex>
   )
 }
