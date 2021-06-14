da19450986272f54c3f61d99d9727ca4e69d5abf
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2021-06-14 08:46:37 -0400

    add current link styles

--- web/src/theme.ts
+++ web/src/theme.ts
@@ -1,10 +1,19 @@
 import {tailwind} from '@theme-ui/presets'
 import {Theme} from 'theme-ui'
 
+console.log({tailwind})
+
 const theme: Theme | any = {
   ...tailwind,
   styles: {
     ...tailwind.styles,
+    a: {
+      ...tailwind.styles.a,
+      current: {
+        color: 'background',
+        bg: 'primary',
+      },
+    },
   },
   alerts: {
     success: {
